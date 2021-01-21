f4ea018dc0b093fae2a8dfacb04e59d64f351644
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2021-01-21 13:49:07 +0100

    test

---
+++ Uppg09_RPS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uppg_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a game of Rock, paper, scissors!\n\nChoose your weapon..\n");
+
+
+            int pickRPS = randomRPS(0,2);
+                
+                switch (pickRPS)
+                {
+                    case 0:
+                        Console.WriteLine("Computer chose ROCK.");
+                        break;
+                    case 1:
+                        Console.WriteLine("Computer chose PAPER.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Computer chose SCISSORS.");
+                        break;
+                }
+
+
+            Console.ReadKey();
+        }
+
+        public static int randomRPS(int minValue, int maxValue)
+        {
+            Random numberGenerator = new Random();
+
+            int rRPS = numberGenerator.Next(minValue, maxValue + 1);
+
+            return rRPS;
+        }
+    }
+}
