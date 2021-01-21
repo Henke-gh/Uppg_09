384087755725f357de5be0ba7beacb945463e565
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2021-01-21 14:32:04 +0100

    testpush

--- Uppg09_RPS.cs
+++ Uppg09_RPS.cs
@@ -7,22 +7,64 @@ namespace Uppg_09
         static void Main(string[] args)
         {
             Console.WriteLine("This is a game of Rock, paper, scissors!\n\nChoose your weapon..\n");
+            Console.WriteLine("Type '1' for Rock, '2' for Paper or '3' for Scissors. Press <ENTER> to confirm.\n");
+
+            int weaponNum = Convert.ToInt32(Console.ReadLine());
+
+            switch (weaponNum)
+            {
+                case 1:
+                    Console.WriteLine("You chose ROCK.");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose PAPER.");
+                    break;
+                case 3:
+                    Console.WriteLine("You chose SCISSORS.");
+                    break;
+            }
 
 
-            int pickRPS = randomRPS(0,2);
-                
-                switch (pickRPS)
-             {
-                     case 0:
-                        Console.WriteLine("Computer chose ROCK.");
-                        break;
-                    case 1:
-                        Console.WriteLine("Computer chose PAPER.");
-                        break;
-                    case 2:
-                        Console.WriteLine("Computer chose SCISSORS.");
-                        break;
-                }
+            int pickRPS = randomRPS(1, 3);
+
+            switch (pickRPS)
+            {
+                case 1:
+                    Console.WriteLine("Computer chose ROCK.");
+                    break;
+                case 2:
+                    Console.WriteLine("Computer chose PAPER.");
+                    break;
+                case 3:
+                    Console.WriteLine("Computer chose SCISSORS.");
+                    break;
+            }
+
+            if (pickRPS == weaponNum)
+            {
+                Console.WriteLine("You tied!");
+            }
+
+            else if (pickRPS == 1 && weaponNum == 2)
+            {
+                Console.WriteLine("You won!");
+            }
+
+            else if (pickRPS == 2 && weaponNum == 3)
+            {
+                Console.WriteLine("You won!");
+            }
+
+            else if (pickRPS == 3 && weaponNum == 1)
+            {
+                Console.WriteLine("You won!");
+            }
+
+            else
+            {
+                Console.WriteLine("You got beat by ones and zeroes..");
+            }
+
 
 
             Console.ReadKey();
